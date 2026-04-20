f59443f1591f2bdedb4856f4aa2961945796e264
to0mas <s2023RehanekTomas@skolabaltaci.cz>
2026-04-20 18:35:53 +0200

    commit

--- lib/screens/standings_screen.dart
+++ lib/screens/standings_screen.dart
@@ -44,15 +44,7 @@ class _StandingsScreenState extends State<StandingsScreen> {
     return Scaffold(
       
       backgroundColor: Colors.transparent,
-    appBar: AppBar(
-  backgroundColor: const Color.fromARGB(150, 197, 11, 11),
-  title: Image.asset(
-    'assets/images/Logo_tr_cerna.png',
-    height: 50,
-    width: 45,
-  ),
-  centerTitle: true,
-),
+ 
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
           : Column(
